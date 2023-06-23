92d4463073f1939314718fbc5a264897d37f218a
Fajar Norman <fajar.norman@mamiteam.com>
2023-06-23 21:20:08 +0700

    init file

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage as</h1>
+    <img src="/spongebob.jpeg" alt="Spongebob" />
+  </div>
+</template>
+
+<style>
+h1 {
+  font-family: "FarAwayGalaxy", sans-serif;
+}
+img {
+  width: 500px;
+}
+</style>
